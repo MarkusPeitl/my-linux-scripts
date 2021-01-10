c9c6128e06b13643e3fa657184f4702402f1dcf9
Markus Peitl <e1526724@student.tuwien.ac.at>
2021-01-10 14:21:58 +0100

    Added install python script as it is a prerequisite

---
+++ install-python.sh
@@ -0,0 +1,14 @@
+sudo apt update
+#sudo apt-get install python3.6
+sudo apt-get install python3.7
+
+# Recent python
+#sudo apt-get install software-properties-common
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt-get update
+#sudo apt-get install python3.8
+
+#sudo apt-get install software-properties-common
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt-get update
+#sudo apt-get install python3.9
